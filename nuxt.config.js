b431417faec03016c1313f8654c8da4b3f27b718
your_username <atiwattum1@gmail.com>
2023-02-24 20:59:19 +0700

    'deploy'

--- nuxt.config.js
+++ nuxt.config.js
@@ -73,7 +73,7 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    baseURL: process.env.NODE_ENV !== 'development' ? process.env.GITPAGES_NAME : '/',
+    baseURL: '/',
     proxy: true
   },
 
