8bd502780f0b7c63a4f583caca050735f1c0224e
your_username <atiwattum1@gmail.com>
2023-02-28 21:31:37 +0700

    'deploy'

--- store/router.js
+++ store/router.js
@@ -1,29 +1,29 @@
-const url = process.env.NODE_ENV == 'development' ? process.env.URL_API_DEV : process.env.URL_API;
+const url = process.env.NODE_ENV == 'development' ? process.env.URL_API_DEV : process.env.URL_API + '/';
 let base_url = process.env.NODE_ENV == 'development' ? '/api/' : process.env.GITPAGES_NAME + 'api/'
 export default {
   service: {
     register: base_url + 'auth/register',
-    option_pets: base_url + 'service/option-pets',
+    option_pets: url + 'service/option-pets',
   },
   users: {
-    list: base_url + 'user',
-    info: base_url + 'user/{:id}',
-    create: base_url + 'user',
-    update: base_url + 'user/{:id}',
-    delete: base_url + 'user/{:id}',
+    list: url + 'user',
+    info: url + 'user/{:id}',
+    create: url + 'user',
+    update: url + 'user/{:id}',
+    delete: url + 'user/{:id}',
   },
   pets: {
-    list: base_url + 'pet/{:user_id}',
-    info: base_url + 'pet/{:user_id}/{:id}',
-    create: base_url + 'pet',
-    update: base_url + 'pet',
-    delete: base_url + 'pet/{:id}',
+    list: url + 'pet/{:user_id}',
+    info: url + 'pet/{:user_id}/{:id}',
+    create: url + 'pet',
+    update: url + 'pet',
+    delete: url + 'pet/{:id}',
   },
   HCP: {
-    list: base_url + 'health-check-pet/{:pet_id}',
-    info: base_url + 'health-check-pet/{:pet_id}/{:id}',
-    create: base_url + 'health-check-pet',
-    update: base_url + 'health-check-pet',
-    delete: base_url + 'health-check-pet/{:id}',
+    list: url + 'health-check-pet/{:pet_id}',
+    info: url + 'health-check-pet/{:pet_id}/{:id}',
+    create: url + 'health-check-pet',
+    update: url + 'health-check-pet',
+    delete: url + 'health-check-pet/{:id}',
   }
 }
